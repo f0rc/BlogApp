583c65d96e0de93485edd95952b27f49840e36cd
0brx <brickmp4@gmail.com>
2021-07-28 15:51:26 -0500

    static files2

--- osowebblog/settings.py
+++ osowebblog/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import json
+#import json
 
-with open('osowebblog\json\config.json') as config_file:
-    config = json.load(config_file)
+#with open('osowebblog\json\config.json') as config_file:
+    #config = json.load(config_file)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = 'django-insecure-#$jsy0t*(7^pvq*ho-p_=osmx(4g5psukdv1oykm)mgiz#tjjy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
